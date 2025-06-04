18a206f95dca7b2a9253e7a9ae053a1cb7c4a4f3
MFuentes1992 <markfuentes2991@gmail.com>
2025-06-04 16:45:17 -0600

    Initial Commit

---
+++ cirkuits-learningTests/cirkuits_learningTests.swift
@@ -0,0 +1,17 @@
+//
+//  cirkuits_learningTests.swift
+//  cirkuits-learningTests
+//
+//  Created by Marco Fuentes Jiménez on 04/06/25.
+//
+
+import Testing
+@testable import cirkuits_learning
+
+struct cirkuits_learningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ cirkuits-learningUITests/cirkuits_learningUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  cirkuits_learningUITestsLaunchTests.swift
+//  cirkuits-learningUITests
+//
+//  Created by Marco Fuentes Jiménez on 04/06/25.
+//
+
+import XCTest
+
+final class cirkuits_learningUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
