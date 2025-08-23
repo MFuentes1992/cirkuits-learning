916d8a66d840f1a6e0cbec9165151a15ebb6767b
MFuentes1992 <markfuentes2991@gmail.com>
2025-08-22 23:22:50 -0600

    Words on screen code refactor

---
+++ cirkuits-learning/Structs/CameraSettings.swift
@@ -0,0 +1,12 @@
+//
+//  PerspectiveCamera.swift
+//  cirkuits-learning
+//
+//  Created by Marco Fuentes Jiménez on 21/08/25.
+//
+import simd
+struct CameraCoordinates {
+    var eye: SIMD3<Float>
+    var center: SIMD3<Float>
+    var up: SIMD3<Float>
+}
